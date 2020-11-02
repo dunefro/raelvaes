0866e7578fa318ada2330bd741f2e6b90849a5c4
Vedant Pareek <dunefro@gmail.com>
2020-11-02 07:53:10 +0000

    Modified imports

--- flask_app/app/v1/__init__.py
+++ flask_app/app/v1/__init__.py
@@ -4,5 +4,5 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-from flask_app.app.v1 import views
-from flask_app.app.v1 import admin_views
+from app.v1 import views
+from app.v1 import admin_views
--- flask_app/app/v1/admin_views.py
+++ flask_app/app/v1/admin_views.py
@@ -1,4 +1,4 @@
-from app import app
+from app.v1 import app
 
 @app.route('/admin/healthz')
 def admin_health():
