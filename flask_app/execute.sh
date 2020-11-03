1e6484a3d9fb5c5745edf63d2a2601bf4ceba9e6
Vedant Pareek <dunefro@gmail.com>
2020-11-03 18:31:48 +0000

    Automatically initiating virtual env

--- flask_app/execute.sh
+++ flask_app/execute.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z $VIRTUAL_ENV ]
+then
+    echo "Activating Python Virtual Environment"
+    source myenv/bin/activate
+fi
 export FLASK_APP=run.py
 export FLASK_ENV=development
 export PYTHONPATH=/home/ubuntu/workspace/raelvaes/flask_app
