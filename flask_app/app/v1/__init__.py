0a05c4066a793732efab4f02fcb86c8839119381
Vedant Pareek <dunefro@gmail.com>
2020-11-03 18:31:17 +0000

    Added encryption-decryption

--- flask_app/app/v1/__init__.py
+++ flask_app/app/v1/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
-from flask_restful import Resource, Api
-from flask_jwt import JWT , jwt_required
+from flask import Flask , request
+from flask_restful import Resource, Api , reqparse
+from flask_jwt import JWT , jwt_required , current_identity
+import os
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 app = Flask(__name__)
+# The secret key is to be initialized before the api object is created
+app.secret_key = 'aR4QpGzGgHcY7haxHT9g89Puph3NgvNBBMBMnHEC'
 api = Api(app)
+salt = b'6o\x96h`\xdf\xf1\xecL2`\xcf=\xf0`\xcc'
 
 from app.v1 import access
-from app.v1 import admin_views
+from app.v1 import health
--- flask_app/app/v1/access.py
+++ flask_app/app/v1/access.py
@@ -1,22 +1,65 @@
-from app.v1 import app , api , Resource , JWT , jwt_required
+from app.v1 import app , api , Resource , JWT , jwt_required , reqparse , request , current_identity , os , base64 , Fernet , hashes , PBKDF2HMAC , salt
 from app.v1.security.auth import authenticate , identity
-api.secret_key = 'aR4QpGzGgHcY7haxHT9g89Puph3NgvNBBMBMnHEC'
 
 jwt = JWT(app , authenticate , identity)
 
 class Access(Resource):
 
+    @staticmethod
+    def decrypt_text(text):
+
+        password = current_identity.password.encode()
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256() , length=32 , salt=salt , iterations=100000)
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        f = Fernet(key)
+        return f.decrypt(text.encode()).decode()
+
     @jwt_required()   
-    def get(self):
-        return 'Works fine'
+    def post(self,name):
+        """This function takes the GET req on /access API. 
+        Used for accessing the VM/cloud over which the authentication will take place either to connect via SSH or deploy VMs. 
 
-api.add_resource(Access,'/access')
+        Args:
+            name ([string]): [kind of authentication. Allowed values are (ssh,aws,gcp,azure,k8s)]
+
+        Returns:
+            [dictionary]: [Will return successful if the authentication works fine.]
+        """
+        if request.is_json:
+            data = request.get_json()
+        else:
+            return {'Message': 'Request Body type is not json'},400
+        decrypted_text = Access.decrypt_text(data['key'])
+        print(decrypted_text)
+        return decrypted_text,200
+
+class SecureText(Resource):
+
+    @staticmethod
+    def encrypt_text(text):
+        password = current_identity.password.encode()
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256() , length=32 , salt=salt , iterations=100000)
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        f = Fernet(key)
+        return f.encrypt(text.encode()).decode()
+
+    @jwt_required()
+    def get(self,text):
+        """Function returns the encrypted text.
+
+        Args:
+            name ([string]): [text that is to be encrypted]
+
+        Returns:
+            [string]: [The encrypted string/token]
+        """
+        try:
+            encrypted_text = SecureText.encrypt_text(text)
+            return {'encrypted': encrypted_text},200
+        except:
+            return {'Message': 'Your text [{}] can\'t be encrypted'.format(text)} , 500
 
 
-# @app.route('/')
-# def index():
-#     return 'Hello World !'
 
-# @app.route('/healthz')
-# def health():
-#     return 'Up and Running !!!'
+api.add_resource(Access,'/access/<string:name>')
+api.add_resource(SecureText,'/securetext/<string:text>')
