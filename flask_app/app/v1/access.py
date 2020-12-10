d326cbcaacd663dcae25f6a8059469063a62f51f
Vedant Pareek <dunefro@gmail.com>
2020-12-10 17:16:37 +0000

    Modified Access.py

--- flask_app/app/v1/access.py
+++ flask_app/app/v1/access.py
@@ -3,35 +3,53 @@ from app.v1.security.auth import authenticate , identity
 
 jwt = JWT(app , authenticate , identity)
 
-class Access(Resource):
+class Host:
 
-    @staticmethod
-    def decrypt_text(text):
+    def __init__(self,**kwargs):
+        self.name = kwargs['name']
+        self.ip = kwargs['ip']
 
-        password = current_identity.password.encode()
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA256() , length=32 , salt=salt , iterations=100000)
-        key = base64.urlsafe_b64encode(kdf.derive(password))
-        f = Fernet(key)
-        return f.decrypt(text.encode()).decode()
+class Access:
 
-    @jwt_required()   
-    def post(self,name):
-        """This function takes the GET req on /access API. 
-        Used for accessing the VM/cloud over which the authentication will take place either to connect via SSH or deploy VMs. 
+    def __init__(self , name, host , access):
+        self.name =  name
+        self.host = Host(host)
+        self.access = access
 
-        Args:
-            name ([string]): [kind of authentication. Allowed values are (ssh,aws,gcp,azure,k8s)]
 
-        Returns:
-            [dictionary]: [Will return successful if the authentication works fine.]
-        """
-        if request.is_json:
-            data = request.get_json()
-        else:
-            return {'Message': 'Request Body type is not json'},400
-        decrypted_text = Access.decrypt_text(data['key'])
-        print(decrypted_text)
-        return decrypted_text,200
+class Access_flask(Resource):
+
+    def post(self , name):
+        data = request.json()
+        print(data)
+
+    # @staticmethod
+    # def decrypt_text(text):
+
+    #     password = current_identity.password.encode()
+    #     kdf = PBKDF2HMAC(algorithm=hashes.SHA256() , length=32 , salt=salt , iterations=100000)
+    #     key = base64.urlsafe_b64encode(kdf.derive(password))
+    #     f = Fernet(key)
+    #     return f.decrypt(text.encode()).decode()
+
+    # @jwt_required()   
+    # def post(self,name):
+    #     """This function takes the GET req on /access API. 
+    #     Used for accessing the VM/cloud over which the authentication will take place either to connect via SSH or deploy VMs. 
+
+    #     Args:
+    #         name ([string]): [kind of authentication. Allowed values are (ssh,aws,gcp,azure,k8s)]
+
+    #     Returns:
+    #         [dictionary]: [Will return successful if the authentication works fine.]
+    #     """
+    #     if request.is_json:
+    #         data = request.get_json()
+    #     else:
+    #         return {'Message': 'Request Body type is not json'},400
+    #     decrypted_text = Access_flask.decrypt_text(data['key'])
+    #     print(decrypted_text)
+    #     return decrypted_text,200
 
 class SecureText(Resource):
 
@@ -61,5 +79,5 @@ class SecureText(Resource):
 
 
 
-api.add_resource(Access,'/access/<string:name>')
+api.add_resource(Access_flask,'/access/<string:name>')
 api.add_resource(SecureText,'/securetext/<string:text>')
